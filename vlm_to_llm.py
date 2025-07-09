9628faf009fcda228035c8ba105e2ea8c5a65980
Euyseok Han <bayer960@gmail.com>
2025-07-10 02:00:54 +0900

    Integrate 2

--- vlm_to_llm.py
+++ vlm_to_llm.py
@@ -1,8 +1,9 @@
 import os
-import subprocess
 from time import time
 from paint_it.paint_it import parse_args, main, StableDiffusion
-
+from gaussian_property_main.folder_organizer import process_images
+from gaussian_property_main.sam_preprocess import sam_model_registry, sam_image, save_gpt_input
+from gaussian_property_main.vlm_predict import run_vlm
 if __name__ == '__main__':
 
     # 1. Paint_it 
@@ -24,7 +25,16 @@ if __name__ == '__main__':
     # main 실행
     main(args, guidance)
 
+    # 2. Folder_organizer
     exp_name = time.strftime('%Y%m%d', time.localtime()) + '_' + args.exp_name
     path_to_preprocess = os.path.join('./logs', exp_name)
-
     preprocessed_save_path = path_to_preprocess + "_dirs"
+    process_images(path_to_preprocess, True)
+
+    # 3. Sam_preprocess
+    sam = sam_model_registry["vit_h"](checkpoint="/local_data_2/urp25su_hanuiseok/sam_vit_h_4b8939.pth").to('cuda')
+    sam_image(sam, preprocessed_save_path)
+    save_gpt_input(preprocessed_save_path)
+
+    # 4. vlm_predict
+    run_vlm(preprocessed_save_path, vlm_type="qwen")
