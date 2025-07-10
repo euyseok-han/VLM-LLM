7be76407370fdf5eea5ee23ea60a8406d3e565c1
Euyseok Han <bayer960@gmail.com>
2025-07-10 13:55:03 +0900

    pipeline

--- vlm_to_llm.py
+++ vlm_to_llm.py
@@ -1,9 +1,11 @@
 import os
-from time import time
+import time
 from paint_it.paint_it import parse_args, main, StableDiffusion
 from gaussian_property_main.folder_organizer import process_images
 from gaussian_property_main.sam_preprocess import sam_model_registry, sam_image, save_gpt_input
 from gaussian_property_main.vlm_predict import run_vlm
+
+
 if __name__ == '__main__':
 
     # 1. Paint_it 
@@ -12,22 +14,26 @@ if __name__ == '__main__':
     args = parse_args()
 
     # 직접 설정 추가 (필요 시)
-    args.objaverse_id = "85819bbcdfee44f8b6f525eb89dd19bd"
-    args.identity = "A chair with tools on it"
+    # args.objaverse_id = "85819bbcdfee44f8b6f525eb89dd19bd"
+    # args.identity = "A chair with tools on it"
+    args.objaverse_id = "d3bfe628db1d47ec9501ee7b5ebce9e3"
+    args.identity = "A wooden building"
+    
     args.exp_name = '_'.join((args.identity.split(' ')[1:] + [args.objaverse_id[:6]]))
 
     # guidance model 불러오기
-    guidance = StableDiffusion("cuda", min=args.sd_min, max=args.sd_max)
-    guidance.eval()
-    for p in guidance.parameters():
-        p.requires_grad = False
+    # guidance = StableDiffusion("cuda", min=args.sd_min, max=args.sd_max)
+    # guidance.eval()
+    # for p in guidance.parameters():
+    #     p.requires_grad = False
 
-    # main 실행
-    main(args, guidance)
+    # # main 실행
+    # main(args, guidance)
 
     # 2. Folder_organizer
     exp_name = time.strftime('%Y%m%d', time.localtime()) + '_' + args.exp_name
     path_to_preprocess = os.path.join('./logs', exp_name)
+    path_to_preprocess = os.path.join(path_to_preprocess, "final_images")
     preprocessed_save_path = path_to_preprocess + "_dirs"
     process_images(path_to_preprocess, True)
 
