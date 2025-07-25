85e7dbb3f071a87b2bc13e98884c15a4b6e8b923
Euyseok Han <bayer960@gmail.com>
2025-07-25 21:57:12 +0900

    d

--- vlm_to_llm.py
+++ vlm_to_llm.py
@@ -16,14 +16,14 @@ if __name__ == '__main__':
 
     args.exp_name = '_'.join((args.identity.split(' ')[1:] + [args.objaverse_id[:6]]))
 
-    # # 1-1. guidance model 불러오기
-    # guidance = StableDiffusion("cuda", min=args.sd_min, max=args.sd_max)
-    # guidance.eval()
-    # for p in guidance.parameters():
-    #     p.requires_grad = False
+    # 1-1. guidance model 불러오기
+    guidance = StableDiffusion("cuda", min=args.sd_min, max=args.sd_max)
+    guidance.eval()
+    for p in guidance.parameters():
+        p.requires_grad = False
 
-    # # 1-2. main 실행
-    # main(args, guidance)
+    # 1-2. main 실행
+    main(args, guidance)
 
     # 2. Folder_organizer
     # 2-2. preprocess_images
@@ -32,14 +32,14 @@ if __name__ == '__main__':
     path_to_preprocess = directory
     preprocessed_save_path = path_to_preprocess + "_dirs"
     print("process image...")
-    process_images(path_to_preprocess, True)
+    # process_images(path_to_preprocess, True)
 
-    # # 3. Sam_preprocess
-    sam = sam_model_registry["vit_h"](checkpoint="/local_data_2/urp25su_hanuiseok/sam_vit_h_4b8939.pth").to('cuda')
-    print("sam image...")
-    sam_image(sam, preprocessed_save_path)
-    print("save gpt input...")
-    save_gpt_input(preprocessed_save_path)
+    # # # 3. Sam_preprocess
+    # sam = sam_model_registry["vit_h"](checkpoint="/local_data_2/urp25su_hanuiseok/sam_vit_h_4b8939.pth").to('cuda')
+    # print("sam image...")
+    # sam_image(sam, preprocessed_save_path)
+    # print("save gpt input...")
+    # save_gpt_input(preprocessed_save_path)
 
     # # 4. vlm_predict
     # print("Feed images to VLM...")
